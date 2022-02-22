9a5bcbf712d223c461b218b3ab1268f5195908f0
pradhanpobitra <pobitra.pradhan.pp@gmail.com>
2022-02-22 21:59:34 +0530

    ...

--- src/main/java/com/cricketgame/repository/HashmapsRepo.java
+++ src/main/java/com/cricketgame/repository/HashmapsRepo.java
@@ -2,7 +2,9 @@ package com.cricketgame.repository;
 
 import com.cricketgame.document.Hashmaps;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HashmapsRepo extends ElasticsearchRepository<Hashmaps, String> {
 
 }
--- src/main/java/com/cricketgame/repository/PlayerRepo.java
+++ src/main/java/com/cricketgame/repository/PlayerRepo.java
@@ -2,7 +2,9 @@ package com.cricketgame.repository;
 
 import com.cricketgame.document.Player;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PlayerRepo extends ElasticsearchRepository<Player, String> {
 
 }
